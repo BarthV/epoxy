e1b76f7551a27a09e69d0c1667c180bdf7446b1c
Barthelemy Vessemont <b.vessemont@criteo.com>
2017-02-07 13:50:58 +0100

    refactor a bit

--- handlers/consulmemcached/consulpoller.go
+++ handlers/consulmemcached/consulpoller.go
@@ -8,24 +8,32 @@ import (
 	"github.com/hashicorp/consul/api"
 )
 
+var consulOptions = api.QueryOptions{}
+
 func ConsulPoller(list *memcache.ServerList) {
 	consulconf := api.DefaultConfig()
 	consulconf.Address = "consul01-par.central.criteo.preprod:8500"
 	consul, _ := api.NewClient(consulconf)
 
-	options := api.QueryOptions{}
-
 	for {
 		cluster := []string{}
-		res, resqry, _ := consul.Catalog().Service("memcached-mesos-test1", "", &options)
+		res, resqry, err := consul.Catalog().Service("memcached-mesos-test1", "", &consulOptions)
+		if err != nil {
+			fmt.Println("Consul Catalog query failed")
+			continue
+		}
 		for _, service := range res {
 			//fmt.Println(service.Node + ":" + strconv.Itoa(service.ServicePort))
 			cluster = append(cluster, service.Node+":"+strconv.Itoa(service.ServicePort))
 		}
 		fmt.Println(">> Cluster update <<")
-		list.SetServers(cluster...)
+		err = list.SetServers(cluster...)
+		if err != nil {
+			fmt.Println("Memcached client serverlist update failed")
+			continue
+		}
 		fmt.Println(cluster)
 		fmt.Println("")
-		options.WaitIndex = resqry.LastIndex
+		consulOptions.WaitIndex = resqry.LastIndex
 	}
 }
